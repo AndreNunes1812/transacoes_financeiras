d96068f56c411a241913dd17cf934bbcbbc3d048
AndreNunes1812 <andre.nunes0207700@gmail.com>
2020-04-19 19:15:52 +0200

    Desafio Finalizado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,65 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+    const categoriesRepository = getRepository(Category);
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError('Tipo Inválido', 401);
+    }
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw new AppError('Saldo Insuficiente...');
+    }
+
+    const categoryExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    let categoryId = categoryExists?.id;
+
+    if (!categoryExists) {
+      const createdCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(createdCategory);
+
+      categoryId = createdCategory.id;
+    }
+
+    const transaction = transactionsRepository.create({
+      category_id: categoryId,
+      value,
+      title,
+      type,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
